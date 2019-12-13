e910726c05ffd74c5d63f37517529c648268fff9
tzwierzy <T.Zwierzynski@wasko.pl>
2019-12-13 13:55:53 +0100

    Dodano do klienta

--- src/client.js
+++ src/client.js
@@ -5,5 +5,8 @@ var socket = io.connect('http://localhost:3000', {reconnect: true});
 // Add a connect listener
 socket.on('connect', function (socket) {
     console.log('Connected!');
-});
-socket.emit('CH01', 'me', 'test msg');
+} );
+
+socket.on('CH02', function (body) {
+	console.log(body);
+} );
